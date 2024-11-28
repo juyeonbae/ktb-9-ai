83b79a148877e3d7e8a747db44238ae88fff21f0
juyeonbae <98314962+juyeonbae@users.noreply.github.com>
2024-11-28 17:27:54 +0900

    Delete tests directory

--- tests/test_download.py
+++
@@ -1,3 +0,0 @@
-def test_download_image():
-    response = client.get("/download/?file_path=test.png")
-    assert response.status_code == 200
--- tests/test_edit.py
+++
@@ -1,3 +0,0 @@
-def test_edit_image():
-    response = client.post("/edit/", json={"command": "객체 제거", "file_path": "test.png"})
-    assert response.status_code == 200
